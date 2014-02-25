b563d66995fbbb13e3fcbc5a123d47f8d6832f8d
mejatysek <mejatysek@gmail.com>
2014-02-25 16:44:24 +0100

    Do testu pridany komentare k assertum.
    !!!! POZOR !!!! seznam se v mojí implementaci indexuje od 1

--- ListIfaceTest.java
+++ ListIfaceTest.java
@@ -10,9 +10,9 @@ public class ListIfaceTest {
     @Test
     public void testAddGet() throws Exception {
         listFill();
-        assertEquals(list.getSize(),6);
+        assertEquals("Velikost listu po přidani 6 polozek neni 6, ale: "+list.getSize(),list.getSize(),6);
         for (int i = 1; i < list.getSize()+1; i++) {
-            assertEquals("Ocekavana: "+(i)+" vracena; "+(int) list.get(i),(int) list.get(i), i);
+            assertEquals("Polozky nejsou ve spravnem poradi, polozka c:"+i+" hodnoty: ocekavana: "+(i)+" vracena; "+(int) list.get(i),(int) list.get(i), i);
         }
     }
 
@@ -20,16 +20,16 @@ public class ListIfaceTest {
     public void testRemove() throws Exception {
         listFill();
         list.remove(1);
-        assertEquals(list.getSize(), 5);
+        assertEquals("Po smazani prvniho prvku nema list velikost 5, ale: "+list.getSize(),list.getSize(), 5);
         list.remove(5);
-        assertEquals(list.getSize(),4);
+        assertEquals("Po smazani posledniho prvku nema list velikost 4, ale: "+list.getSize(),list.getSize(),4);
         list.remove(2);
-        assertEquals(list.getSize(),3);
+        assertEquals("Po smazani druheho prvku nema list velikost 3, ale: "+list.getSize(),list.getSize(),3);
         for (int i = 1; i < list.getSize()+1; i++) {
             if(i>1){
-                assertEquals("Ocekavana: "+(i+2)+" vracena; "+(int) list.get(i),(int) list.get(i), i+2); 
+                assertEquals("Smazana spatná polozka. Hodnoty: ocekavana: "+(i+2)+" vracena; "+(int) list.get(i),(int) list.get(i), i+2);
             }else{
-            assertEquals("Ocekavana: "+(i+1)+" vracena; "+(int) list.get(i),(int) list.get(i), i+1);
+            assertEquals("Smazana spatná polozka. Hodnoty: ocekavana: "+(i+1)+" vracena; "+(int) list.get(i),(int) list.get(i), i+1);
             }
         }
     }
@@ -37,11 +37,11 @@ public class ListIfaceTest {
     @Test
     public void testGetSize() throws Exception {
         list= new Jednosmerny<Integer>();
-        assertEquals(list.getSize(),0);
+        assertEquals("Prazdny list nema velikost 0, ale: "+list.getSize(),list.getSize(),0);
         list.add(1);
-        assertEquals(list.getSize(),1);
+        assertEquals("List s jednim prvkem nema velikost 1, ale: "+list.getSize(),list.getSize(),1);
         list.add(2);
-        assertEquals(list.getSize(),2);
+        assertEquals("List s dma prvky nema velikost 2"+list.getSize(),list.getSize(),2);
 
     }
     private void listFill() {
