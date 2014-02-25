f1f3cf4c8ed4a99483001d4c200c466e9d6cbee5
Lukáš Tomčík <tomciluk@fel.cvut.cz>
2014-02-25 21:15:48 +0100

    Do JUnit testu pridana podpora pro zero indexed Linked Listy

--- ListIfaceTest.java
+++ ListIfaceTest.java
@@ -1,17 +1,23 @@
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by mejty on 25.2.14.
+ * Rozhrani, jez definuje operace, ktere musi splnit kazda implementace seznamu
+ * @author Matej Mihal
+ * @contributor Lukas Tomcik
  */
 public class ListIfaceTest {
     private ListIface<Integer> list;
+    /**
+     * Pokud ve svem LinkedListu indexujete od nuly zmente INITIAL_INDEX_POSITION na 0
+     */
+    private final int INITIAL_INDEX_POSITION = 1;
+
     @Test
     public void testAddGet() throws Exception {
         listFill();
         assertEquals("Velikost listu po přidani 6 polozek neni 6, ale: "+list.getSize(),list.getSize(),6);
-        for (int i = 1; i < list.getSize()+1; i++) {
+        for (int i = INITIAL_INDEX_POSITION; i < list.getSize()+INITIAL_INDEX_POSITION; i++) {
             assertEquals("Polozky nejsou ve spravnem poradi, polozka c:"+i+" hodnoty: ocekavana: "+(i)+" vracena; "+(int) list.get(i),(int) list.get(i), i);
         }
     }
@@ -19,17 +25,17 @@ public class ListIfaceTest {
     @Test
     public void testRemove() throws Exception {
         listFill();
-        list.remove(1);
+        list.remove(INITIAL_INDEX_POSITION);
         assertEquals("Po smazani prvniho prvku nema list velikost 5, ale: "+list.getSize(),list.getSize(), 5);
-        list.remove(5);
+        list.remove(4+INITIAL_INDEX_POSITION);
         assertEquals("Po smazani posledniho prvku nema list velikost 4, ale: "+list.getSize(),list.getSize(),4);
         list.remove(2);
         assertEquals("Po smazani druheho prvku nema list velikost 3, ale: "+list.getSize(),list.getSize(),3);
-        for (int i = 1; i < list.getSize()+1; i++) {
-            if(i>1){
-                assertEquals("Smazana spatná polozka. Hodnoty: ocekavana: "+(i+2)+" vracena; "+(int) list.get(i),(int) list.get(i), i+2);
+        for (int i = INITIAL_INDEX_POSITION; i < list.getSize()+INITIAL_INDEX_POSITION; i++) {
+            if(i>INITIAL_INDEX_POSITION){
+                assertEquals("Smazana spatná polozka. Hodnoty: ocekavana: "+(i+1+INITIAL_INDEX_POSITION)+" vracena; "+(int) list.get(i),(int) list.get(i), i+1+INITIAL_INDEX_POSITION);
             }else{
-            assertEquals("Smazana spatná polozka. Hodnoty: ocekavana: "+(i+1)+" vracena; "+(int) list.get(i),(int) list.get(i), i+1);
+            assertEquals("Smazana spatná polozka. Hodnoty: ocekavana: "+(i+INITIAL_INDEX_POSITION)+" vracena; "+(int) list.get(i),(int) list.get(i), i+INITIAL_INDEX_POSITION);
             }
         }
     }
@@ -44,6 +50,7 @@ public class ListIfaceTest {
         assertEquals("List s dma prvky nema velikost 2"+list.getSize(),list.getSize(),2);
 
     }
+    
     private void listFill() {
         list=new Jednosmerny<Integer>();
         list.add(1);
